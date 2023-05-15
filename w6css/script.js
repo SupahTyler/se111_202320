7b4175ea4097d0308e05eb975cc0f328f405a7fb
Tyler Uttley <tjuttley@email.neit.edu>
2023-05-15 03:45:10 -0400

    week6css

---
+++ w6css/script.js
@@ -0,0 +1,28 @@
+var toggle = document.querySelector(`#toggle`)
+var tray = document.querySelector(`.tray`)
+
+var tabs = Array.from(document.querySelectorAll(`nav a`))
+var p = document.querySelector(`#editor p`)
+
+toggle.addEventListener(`click`, (e)=>{
+    e.preventDefault();
+    tray.classList.toggle(`closed`)
+})
+
+for(let i=0; i<tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, selectTab);
+}
+
+function selectTab(e)
+{
+    for(let i=0; i<tabs.length; i++)
+    {
+        tabs[i].style.backgroundColor =`gray`;
+    }
+    e.target.style.backgroundColor=`gray`;
+    p.innerHTML =  `<a href=#>SE 137</a> >> 
+                    <a href=#>W6</a> >>
+                    <a href=#>Homework</a> >>
+                    <a href=#>Tab ${tabs.indexOf(e.target)+1}</a>`
+}
